07de6c1a60dfaa64565cf8833591f430e036ff6c
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-10-16 11:01:33 +0200

    [BEAM-1920] Add a spark1/spark2 compatible version of Spark's BlockManager
    It also refactors Spark runner's GlobalWatermarkHoler to use the
    spark1/spark2 compatible version of BlockManager.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/util/SparkCompat.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/util/SparkCompat.java
@@ -18,8 +18,17 @@
 
 package org.apache.beam.runners.spark.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
+import java.util.Map;
+import org.apache.spark.SparkEnv;
+import org.apache.spark.storage.BlockId;
+import org.apache.spark.storage.BlockResult;
+import org.apache.spark.storage.StorageLevel;
+import scala.Option;
 import scala.Tuple2;
+import scala.reflect.ClassTag;
 
 /** A class with helper functions and utilities for compatibility between spark versions. */
 public final class SparkCompat {
@@ -102,5 +111,131 @@ public final class SparkCompat {
 
     @Override
     SparkCompat.IterableIterator<Tuple2<K, V>> call(T t) throws Exception;
+  }
+
+  /**
+   * A spark 1/2 compatible version of the Spark's {@link org.apache.spark.storage.BlockManager}.
+   */
+  abstract static class BlockManager {
+    static final BlockId WATERMARKS_BLOCK_ID = BlockId.apply("broadcast_0WATERMARKS");
+
+    void removeBlock(BlockId blockId, boolean tellMaster) {
+      final SparkEnv sparkEnv = SparkEnv.get();
+      if (sparkEnv != null) {
+        final org.apache.spark.storage.BlockManager blockManager = sparkEnv.blockManager();
+        blockManager.removeBlock(WATERMARKS_BLOCK_ID, true);
+      }
+    }
+
+    abstract void putSingle(Map<Integer, GlobalWatermarkHolder.SparkWatermarks> empty);
+
+    abstract Option<BlockResult> getRemote();
+  }
+
+  /**
+   * A Factory Method that detects the version of Spark and creates and appropriate BlockManager
+   * instance.
+   */
+  static BlockManager newBlockManager() {
+    final String sparkVersion = org.apache.spark.package$.MODULE$.SPARK_VERSION();
+    return sparkVersion.startsWith("2.") ? new Spark2BlockManager() : new Spark1BlockManager();
+  }
+
+  private static class Spark2BlockManager extends BlockManager {
+
+    private static final ClassTag<Map> WATERMARKS_TAG =
+        scala.reflect.ClassManifestFactory.fromClass(Map.class);
+
+    private final Method putSingle;
+    private final Method getRemote;
+
+    private Spark2BlockManager() {
+      try {
+        this.putSingle =
+            org.apache.spark.storage.BlockManager.class.getMethod(
+                "putSingle",
+                BlockId.class,
+                Object.class,
+                StorageLevel.class,
+                boolean.class,
+                ClassTag.class);
+        this.getRemote =
+            org.apache.spark.storage.BlockManager.class.getMethod(
+                "get", BlockId.class, ClassTag.class);
+      } catch (NoSuchMethodException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+
+    @Override
+    public void putSingle(Map<Integer, GlobalWatermarkHolder.SparkWatermarks> empty) {
+      try {
+        putSingle.invoke(
+            SparkEnv.get().blockManager(),
+            new Object[] {
+              WATERMARKS_BLOCK_ID, empty, StorageLevel.MEMORY_ONLY(), true, WATERMARKS_TAG
+            });
+      } catch (IllegalAccessException e) {
+        throw new IllegalStateException(e);
+      } catch (InvocationTargetException e) {
+        throw new IllegalStateException(e.getTargetException());
+      }
+    }
+
+    @Override
+    public Option<BlockResult> getRemote() {
+      try {
+        return Option.class.cast(
+            getRemote.invoke(
+                SparkEnv.get().blockManager(), new Object[] {WATERMARKS_BLOCK_ID, WATERMARKS_TAG}));
+      } catch (IllegalAccessException e) {
+        throw new IllegalStateException(e);
+      } catch (InvocationTargetException e) {
+        throw new IllegalStateException(e.getTargetException());
+      }
+    }
+  }
+
+  private static class Spark1BlockManager extends BlockManager {
+
+    private final Method putSingle;
+    private final Method getRemote;
+
+    private Spark1BlockManager() {
+      try {
+        this.putSingle =
+            org.apache.spark.storage.BlockManager.class.getMethod(
+                "putSingle", BlockId.class, Object.class, StorageLevel.class, boolean.class);
+        this.getRemote =
+            org.apache.spark.storage.BlockManager.class.getMethod("getRemote", BlockId.class);
+      } catch (NoSuchMethodException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+
+    @Override
+    public void putSingle(Map<Integer, GlobalWatermarkHolder.SparkWatermarks> empty) {
+      try {
+        putSingle.invoke(
+            SparkEnv.get().blockManager(),
+            new Object[] {WATERMARKS_BLOCK_ID, empty, StorageLevel.MEMORY_ONLY(), true});
+      } catch (IllegalAccessException e) {
+        throw new IllegalStateException(e);
+      } catch (InvocationTargetException e) {
+        throw new IllegalStateException(e.getTargetException());
+      }
+    }
+
+    @Override
+    public Option<BlockResult> getRemote() {
+      try {
+        return Option.class.cast(
+            getRemote.invoke(SparkEnv.get().blockManager(), new Object[] {WATERMARKS_BLOCK_ID}));
+      } catch (IllegalAccessException e) {
+        throw new IllegalStateException(e);
+      } catch (InvocationTargetException e) {
+        throw new IllegalStateException(e.getTargetException());
+      }
+    }
   }
 }
